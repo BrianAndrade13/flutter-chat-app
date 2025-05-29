b4eef72632b3a3e0dcedd5268b71c1526c6b7849
BrianAndrade13 <brianandrade.m13@gmail.com>
2025-05-28 20:10:25 -0500

    Fin Seccion 7

--- lib/pages/chat_pages.dart
+++ lib/pages/chat_pages.dart
@@ -1,18 +1,150 @@
+import 'dart:io';
+
+import 'package:chat_app/widgets/chat_message.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPages extends StatelessWidget {
+class ChatPages extends StatefulWidget {
   const ChatPages({super.key});
+
+  @override
+  State<ChatPages> createState() => _ChatPagesState();
+}
+
+class _ChatPagesState extends State<ChatPages> with TickerProviderStateMixin {
+
+  final _textController = new TextEditingController();
+  final _focusNode = new FocusNode();
+
+  final List<ChatMessage> _message = [
+  ];
+
+  bool _estaEscribiendo = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat Page'),
-      centerTitle: true,
+      backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            CircleAvatar(
+              child: Text('Te', style: TextStyle(fontSize: 12, color: Colors.white)),
+              backgroundColor: Colors.blue,
+              maxRadius: 14,
+            ),
+            SizedBox(height: 3),
+            Text('Tenorio', style: TextStyle(color: Colors.black87, fontSize: 20))
+          ],
+        ),
+          centerTitle: true,
+          elevation: 1,
       ),
-      body: Center(
-        child: Text('Hola Mundo'),
-      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemCount: _message.length,
+                itemBuilder: (_, i) => _message[i],
+                reverse: true,
+              )),
+
+              Divider(height: 1),
+
+              //TODO: caja de texto
+              Container(
+                color: Colors.white,
+                child: _inputChat(),
+              ),
+
+          ],
+        ),
+      )
     );
   }
+
+  Widget _inputChat() {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                cursorColor: Colors.blue,
+                controller: _textController,
+                onSubmitted: _handlerSubmit,
+                onChanged: (String texto) {
+                  setState(() {
+                    if(texto.trim().length > 0 ) {
+                      _estaEscribiendo = true; 
+                    } else {
+                      _estaEscribiendo = false;
+                    }
+                  });
+                },
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Enviar Mensaje',
+                ),
+                focusNode: _focusNode,
+              )),
+
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: Platform.isIOS
+                ? CupertinoButton(
+                  child: Text('Enviar'), 
+                  onPressed: _estaEscribiendo
+                      ? () => _handlerSubmit(_textController.text.trim())
+                      : null,
+                  )
+                : Container(
+                  margin: EdgeInsets.symmetric(horizontal: 4.0),
+                  child: IconTheme(
+                    data: IconThemeData(color: Colors.blue[400]),
+                    child: IconButton( 
+                      highlightColor: Colors.transparent,
+                      splashColor: Colors.transparent,
+                      icon: Icon(Icons.send),
+                      onPressed: _estaEscribiendo
+                      ? () => _handlerSubmit(_textController.text.trim())
+                      : null,
+                      ),
+                  ),
+                ) 
+              )
+          ],
+        ),
+    ));
+  }
+
+  _handlerSubmit(String texto) {
+    
+    if(texto.length == 0) return;
+
+    print(texto);
+    _textController.clear();
+    _focusNode.requestFocus();
+
+    final newMessage = new ChatMessage(uid: '123', texto: texto, animationController: AnimationController(vsync: this, duration: Duration(milliseconds: 400)));
+    _message.insert(0, newMessage);
+    newMessage.animationController.forward();
+
+    setState(() {
+      _estaEscribiendo = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: Off del Socket
+    for(ChatMessage message in _message) {
+      message.animationController.dispose();
+    }
+
+    super.dispose();
+  }
+
 }
--- lib/pages/usuarios_pages.dart
+++ lib/pages/usuarios_pages.dart
@@ -1,18 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class UsuariosPages extends StatelessWidget {
+import '../models/usuario.dart';
+
+class UsuariosPages extends StatefulWidget {
   const UsuariosPages({super.key});
+
+  @override
+  State<UsuariosPages> createState() => _UsuariosPagesState();
+}
+
+class _UsuariosPagesState extends State<UsuariosPages> {
+
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
+
+  final usuarios = [
+    Usuario(online: true, email: 'test1@gmail.com', nombre: 'Brian', uid: '1'),
+    Usuario(online: false, email: 'test2@gmail.com', nombre: 'Enric', uid: '2'),
+    Usuario(online: true, email: 'test3@gmail.com', nombre: 'Andrade', uid: '3')
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Usuarios Page'),
+        backgroundColor: const Color.fromARGB(221, 20, 23, 221),
+        title: Text('Mi Nombre', style: TextStyle(color: Colors.white),),
+        elevation: 1,
       centerTitle: true,
+      leading: IconButton(
+        onPressed: () {}, 
+        icon: Icon(Icons.exit_to_app, color: Colors.white)),
+      actions: [
+        Container(
+          margin: EdgeInsets.only(right: 10),
+          // child: Icon(Icons.check_circle, color: Colors.green),
+          child: Icon(Icons.offline_bolt, color: Colors.red),
+        )
+      ],
       ),
-      body: Center(
-        child: Text('Hola Mundo'),
-      ),
+      body: SmartRefresher(
+        controller: _refreshController,
+        enablePullDown: true,
+        onRefresh: _cargarUsuarios,
+        header: WaterDropHeader(
+          complete: Icon(Icons.check, color: Colors.blue),
+          waterDropColor: Colors.blue,
+        ),
+        child: _listViewUsuarios(),
+        )
     );
+  }
+
+  ListView _listViewUsuarios() {
+    return ListView.separated(
+      physics: BouncingScrollPhysics(),
+      separatorBuilder: (_, i) => Divider(), 
+      itemCount: usuarios.length,
+      itemBuilder: (_, i) => _usuariosListTile(usuarios[i]), 
+      );
+  }
+
+  ListTile _usuariosListTile(Usuario usuario) {
+    return ListTile(
+        title: Text(usuario.nombre),
+        subtitle: Text(usuario.email),
+        leading: CircleAvatar(
+          backgroundColor: Color.fromARGB(221, 20, 23, 221),
+          child: Text(usuario.nombre.substring(0,2), style: TextStyle(color: Colors.white)),
+        ),
+        trailing: Container(
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+            color: usuario.online ? Colors.green : Colors.red,
+            borderRadius: BorderRadius.circular(100)
+           ),
+        ),
+      );
+  }
+
+  _cargarUsuarios() async {
+       // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
   }
 }
